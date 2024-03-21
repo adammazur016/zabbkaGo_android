9147b7133d2a2c938f55919e137032039d3e754e
adammazur016 <adam.mazur016@gmail.com>
2024-03-21 18:33:24 +0100

    added location tracking

--- app/src/main/java/com/adayup/zabbkago/maps.kt
+++ app/src/main/java/com/adayup/zabbkago/maps.kt
@@ -1,22 +1,161 @@
 package com.adayup.zabbkago
 
+import android.Manifest
+import android.content.IntentSender
+import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
+import android.graphics.Color
+import android.location.Address
+import android.location.Geocoder
+import android.location.Location
+import android.os.Build.VERSION_CODES.M
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Looper
+import android.util.Log
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.LocationSource.OnLocationChangedListener
+import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.CircleOptions
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
-class maps : AppCompatActivity() {
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationSettingsRequest
+import com.google.android.gms.location.LocationSettingsStatusCodes
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.CameraPosition
+import java.io.IOException
+import java.util.Locale
+import java.util.concurrent.TimeUnit
+
+class maps : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+
+    private lateinit var currentLocation: Location
+
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private val permissionCode = 101
+
+    private lateinit var locationRequest: LocationRequest
+    private lateinit var locationCallback: LocationCallback
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
 
-        val mapFragment = supportFragmentManager.findFragmentById(R.id.map_fragment) as? SupportMapFragment
-        mapFragment?.getMapAsync { googleMap ->
-            val sydney = LatLng(-34.00, 151.00)
-            googleMap.addMarker(MarkerOptions().position(sydney).title("Sydney"))
-            googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+
+        locationRequest = LocationRequest.create().apply {
+            interval = 1000 // Request location update every 10 seconds
+            fastestInterval = 500 // The fastest interval for location updates, 5 seconds
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        }
+
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult) {
+                for (location in locationResult.locations) {
+                    // Handle the new location update here
+                    Log.d("chuuuhj", "chuuuuj")
+                    currentLocation = location
+                    updateMapLocation(location) // You'll implement this method next
+                }
+            }
+        }
+
+        getCurrentLocationUser()
+    }
+
+    private fun updateMapLocation(location: Location) {
+        val latLng = LatLng(location.latitude, location.longitude)
+        mMap.clear() // Clear the previous location marker
+        mMap.addMarker(MarkerOptions().position(latLng).title("Current Location"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 18f))
+        mMap?.addCircle(CircleOptions().center(latLng).strokeColor(Color.GREEN).radius(30.0))
+    }
+
+    private fun getCurrentLocationUser() {
+        if(ActivityCompat.checkSelfPermission(
+                this, android.Manifest.permission.ACCESS_FINE_LOCATION)!=PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+
+            ActivityCompat.requestPermissions(this,
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), permissionCode)
+            return
+        }
+
+        val getLocation = fusedLocationProviderClient.lastLocation.addOnSuccessListener {
+
+                location ->
+
+            if(location != null){
+                currentLocation = location
+
+                Toast.makeText(applicationContext, currentLocation.latitude.toString()+" " + currentLocation.longitude.toString(), Toast.LENGTH_LONG).show()
+
+                val mapFragment = supportFragmentManager.findFragmentById(R.id.map_fragment) as? SupportMapFragment
+                mapFragment?.getMapAsync(this)
+
+            }
         }
     }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode){
+
+            permissionCode -> if(grantResults.isEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                getCurrentLocationUser()
+            }
+        }
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        // SETTING THE LOCATION TO ADD TO MAP TO USER LOCATION
+        mMap = googleMap
+
+        updateMapLocation(currentLocation)
+
+
+    }
+
+    private fun startLocationUpdates() {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), permissionCode)
+            return
+        }
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper())
+    }
+
+    private fun stopLocationUpdates() {
+        fusedLocationProviderClient.removeLocationUpdates(locationCallback)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        startLocationUpdates()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        stopLocationUpdates()
+    }
+
+
 }
