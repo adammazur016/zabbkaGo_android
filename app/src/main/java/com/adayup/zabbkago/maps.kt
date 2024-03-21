81e98e5590c4ba46e95f4d943f11dbab5f4a6bc4
adammazur016 <adam.mazur016@gmail.com>
2024-03-21 22:31:48 +0100

    implemented checking of the point area, added icons

--- app/src/main/java/com/adayup/zabbkago/maps.kt
+++ app/src/main/java/com/adayup/zabbkago/maps.kt
@@ -1,6 +1,7 @@
 package com.adayup.zabbkago
 
 import android.Manifest
+import android.content.Intent
 import android.content.IntentSender
 import android.content.pm.PackageManager
 import android.graphics.BitmapFactory
@@ -17,6 +18,7 @@ import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.dynamic.IObjectWrapper
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -33,13 +35,17 @@ import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationResult
 import com.google.android.gms.location.LocationSettingsRequest
 import com.google.android.gms.location.LocationSettingsStatusCodes
+import com.google.android.gms.maps.GoogleMap.OnMarkerClickListener
+import com.google.android.gms.maps.model.BitmapDescriptor
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.Marker
 import java.io.IOException
 import java.util.Locale
 import java.util.concurrent.TimeUnit
+import kotlin.math.sqrt
 
-class maps : AppCompatActivity(), OnMapReadyCallback {
+class maps : AppCompatActivity(), OnMapReadyCallback, OnMarkerClickListener {
 
     private lateinit var mMap: GoogleMap
 
@@ -58,8 +64,8 @@ class maps : AppCompatActivity(), OnMapReadyCallback {
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
 
         locationRequest = LocationRequest.create().apply {
-            interval = 1000 // Request location update every 10 seconds
-            fastestInterval = 500 // The fastest interval for location updates, 5 seconds
+            interval = 3000 // Request location update every 10 seconds
+            fastestInterval = 2000 // The fastest interval for location updates, 5 seconds
             priority = LocationRequest.PRIORITY_HIGH_ACCURACY
         }
 
@@ -80,9 +86,19 @@ class maps : AppCompatActivity(), OnMapReadyCallback {
     private fun updateMapLocation(location: Location) {
         val latLng = LatLng(location.latitude, location.longitude)
         mMap.clear() // Clear the previous location marker
-        mMap.addMarker(MarkerOptions().position(latLng).title("Current Location"))
+        mMap.addMarker(MarkerOptions().position(latLng).title("Current Location").icon(BitmapDescriptorFactory.fromResource(R.drawable.test50)))
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 18f))
-        mMap?.addCircle(CircleOptions().center(latLng).strokeColor(Color.GREEN).radius(30.0))
+        mMap.addCircle(CircleOptions().center(latLng).strokeColor(Color.rgb(4, 237, 0)).radius(30.0).fillColor(Color.rgb(194, 250, 192)))
+
+        val test1 = LatLng(50.754529, 17.616104)
+        mMap.addMarker(MarkerOptions().position(test1).title("test marker").icon(BitmapDescriptorFactory.fromResource(R.drawable.markericon50)))
+    }
+
+    private fun getVectorLength(x1: Double, x2: Double, y1: Double, y2: Double): Double{
+        val x12 = x2-x1
+        val y12 = y2-y1
+
+        return sqrt( x12*x12 + y12*y12 )
     }
 
     private fun getCurrentLocationUser() {
@@ -101,8 +117,6 @@ class maps : AppCompatActivity(), OnMapReadyCallback {
 
             if(location != null){
                 currentLocation = location
-
-                Toast.makeText(applicationContext, currentLocation.latitude.toString()+" " + currentLocation.longitude.toString(), Toast.LENGTH_LONG).show()
 
                 val mapFragment = supportFragmentManager.findFragmentById(R.id.map_fragment) as? SupportMapFragment
                 mapFragment?.getMapAsync(this)
@@ -128,9 +142,9 @@ class maps : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         // SETTING THE LOCATION TO ADD TO MAP TO USER LOCATION
         mMap = googleMap
+        mMap.setOnMarkerClickListener(this)
 
         updateMapLocation(currentLocation)
-
 
     }
 
@@ -155,6 +169,20 @@ class maps : AppCompatActivity(), OnMapReadyCallback {
     override fun onPause() {
         super.onPause()
         stopLocationUpdates()
+    }
+
+    override fun onMarkerClick(myMarker: Marker): Boolean {
+        val vectorLength: Double = getVectorLength(currentLocation.latitude, myMarker.position.latitude, currentLocation.longitude, myMarker.position.longitude)
+
+        var toastText: String
+        if(vectorLength >= 3.5848094149922953E-4){
+            toastText = "Come closer to that point!"
+        } else {
+            toastText = "Point collected!"
+        }
+
+        Toast.makeText(applicationContext, "CLICKED ON MARKER: " + myMarker.title + " - " + toastText, Toast.LENGTH_LONG).show()
+        return true
     }
 
 
